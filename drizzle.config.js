b5935663cd07253e6c644505c034fd3d22773043
Atharva A Deshmukh <atharvad1775@gmail.com>
2025-04-17 08:49:43 +0530

    d---b

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials:{
-    url:'postgresql://lmsacc_owner:npg_tSU7ZjA5CoID@ep-muddy-river-a5w1n83c-pooler.us-east-2.aws.neon.tech/lmsacc?sslmode=require'
+    url:NEXT_PUBLIC_DATABASE_CONNECTION_STRING
   }
 });
