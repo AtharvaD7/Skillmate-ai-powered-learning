61659ae5abd6fdd9886a5588cf10c233f57d6f34
Atharva A Deshmukh <atharvad1775@gmail.com>
2025-06-16 13:06:10 +0530

    Chat User

--- app/api/chat-user/route.js
+++ app/api/chat-user/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
   try {
     const { message } = await req.json();
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
 
     // Context aware
     const chat = model.startChat({
