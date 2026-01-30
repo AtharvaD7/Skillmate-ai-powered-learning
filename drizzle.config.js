9c0640e737d7493e585ecd6e8bf46188eff0f226
AtharvaD7 <atharvad1775@gmail.com>
2026-01-30 20:11:54 +0530

    Secure Patch

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,13 +1,1 @@
-import { defineConfig } from "drizzle-kit";
-import { config } from 'dotenv';
-import { resolve } from 'path';
 
-
-config({ path: resolve(__dirname, '.env.local') });
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./configs/schema.js",
-  dbCredentials: {
-    url: process.env.DATABASE_CONNECTION_STRING
-  }
-});
